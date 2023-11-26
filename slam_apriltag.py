b8c2d4236f9e565e54607e225962a4262a4936ea
Manav Gagvani <mgagvani@outlook.com>
2023-11-26 17:48:26 -0500

    strip down example code to just getting pose

--- slam_apriltag.py
+++ slam_apriltag.py
@@ -42,108 +42,34 @@ def make_pipelines(aprilTagPath="apriltag_configs/test_apriltags.json"):
 
     return pipeline, vio_pipeline
 
-def make_camera_wireframe(aspect=640/400., scale=0.0025):
-    # camera "frustum"
-    corners = [[-1, -1], [1, -1], [1, 1], [-1, 1], [-1, -1]]
-    cam_wire = []
-    for x, y in corners:
-        cam_wire.append([x*aspect, y, 1])
-    for x, y in corners:
-        cam_wire.append([x*aspect, y, 1])
-        cam_wire.append([0, 0, 0])
-    return (scale * np.array(cam_wire)).tolist()
-
-class MatplotlibVisualization:
+class ApriltagLocalizer:
     def __init__(self):
-        from mpl_toolkits.mplot3d import Axes3D
-        from matplotlib.animation import FuncAnimation
-
         self.active = True
-
-        fig = plt.figure()
-        ax = Axes3D(fig, auto_add_to_figure=False)
-        fig.add_axes(ax)
-
-        ax_bounds = (-5, 5) # meters
-        ax.set(xlim=ax_bounds, ylim=ax_bounds, zlim=ax_bounds)
-        ax.view_init(azim=-140) # initial plot orientation
 
         empty_xyz = lambda: { c: [] for c in 'xyz' }
 
         vio_data = empty_xyz()
-        vio_data['plot'] = ax.plot(
-            xs=[], ys=[], zs=[],
-            linestyle="-",
-            marker="",
-            label='VIO trajectory'
-        )
-
-        vio_cam_data = empty_xyz()
-        vio_cam_data['plot'] = ax.plot(
-            xs=[], ys=[], zs=[],
-            linestyle="-",
-            marker="",
-            label='current cam pose'
-        )
-
-        ax.legend()
-        ax.set_xlabel("x (m)")
-        ax.set_ylabel("y (m)")
-        ax.set_zlabel("z (m)")
 
         def on_close(*args):
             self.should_close = True
 
-        fig.canvas.mpl_connect('close_event', on_close)
-
-        self.cam_wire = make_camera_wireframe(scale=0.025)
         self.vio_data = vio_data
-        self.vio_cam_data = vio_cam_data
-        self.should_close = False
 
-        def update_graph(*args):
-            r = []
-            for graph in [self.vio_data, self.vio_cam_data]:
-                p = graph['plot'][0]
-                x, y, z = [np.array(graph[c]) for c in 'xyz']
-                p.set_data(x, y)
-                p.set_3d_properties(z)
-                r.append(p)
-            return tuple(r)
-
-        self._anim = FuncAnimation(fig, update_graph, interval=15, blit=True)
-
-    def print_xyz_rot(self, pose: spectacularAI.Pose):
+    def print_xyz_rot(self, pose: spectacularAI.Pose, ending="\n"):
         position = pose.position
         x, y, z = position.x, position.y, position.z
 
         rotation = pose.orientation
         rw, rx, ry, rz = rotation.w, rotation.x, rotation.y, rotation.z
 
-        print(f"({x}, {y}, {z}), ({rw}, {rx}, {ry}, {rz})")
+        print(f"({x}, {y}, {z}), ({rw}, {rx}, {ry}, {rz})", end=ending)
     
-    def update_vio(self, vio_out):
-        if self.should_close: return False
-        view_mat = vio_out.pose.asMatrix()
-
-        for c in 'xyz': self.vio_cam_data[c] = []
-        for vertex in self.cam_wire:
-            p_local = np.array(vertex + [1])
-            p_world = (view_mat @ p_local)[:3]
-            for i, c in enumerate('xyz'):
-                self.vio_cam_data[c].append(p_world[i])
-
-        for c in 'xyz':
-            self.vio_data[c].append(getattr(vio_out.pose.position, c))
-
-        # debug print XYZ
-        self.print_xyz_rot(vio_out.pose)
-
-        return True
-
-    def clear(self):
-        self.active = False
-        for c in 'xyz': del self.vio_data[c][:]
+    def print_vio(self, vio_out):
+        '''
+        - don't use (kept it for reference of `asMatrix())`
+        '''
+        view_mat = vio_out.pose.asMatrix() # 4x4 matrix (dont need this)
+        return DeprecationWarning
 
     def start_in_parallel_with(self, parallel_thing):
         thread = threading.Thread(target = parallel_thing)
@@ -157,27 +83,39 @@ if __name__ == '__main__':
     with depthai.Device(pipeline) as device, \
         vio_pipeline.startSession(device) as vio_session:
 
-        visu_3d = MatplotlibVisualization()
+        visu_3d = ApriltagLocalizer()
 
         def main_loop():
             rgbQueue = device.getOutputQueue(name="rgb", maxSize=4, blocking=False)
 
+            timing_info = []
+            counter = 0
+
             while True:
-                if vio_session.hasOutput():
-                    vio_out = vio_session.getOutput()
-                    if not visu_3d.update_vio(vio_out): break
-                elif rgbQueue.has():
+                t1 = time.perf_counter()
+                ### VIO ###
+                if vio_session.hasOutput(): # if we have a new vio output
+                    vio_out = vio_session.getOutput() # get it
+                    visu_3d.print_xyz_rot(vio_out.pose, ending="\r")
+                elif rgbQueue.has(): # if we have a new rgb frame
                     rgbFrame = rgbQueue.get()
 
-                    if SHOW_CAM:
+                    if SHOW_CAM: # debug viewer (so we know if apriltag in frame)
                         cv2.imshow("rgb", rgbFrame.getCvFrame())
 
                         cv_key = cv2.waitKey(1)
                         if cv_key == ord('q'):
                             break
-                        elif cv_key == ord('c'): # for clear
-                            visu_3d.clear()
-                else:
-                    time.sleep(0.005)
+                else: # if we have neither a new vio output nor a new rgb frame
+                    time.sleep(0.005) # need to sleep to not race condition
+
+                ### VIO ###
+                dt = time.perf_counter() - t1
+                timing_info.append(dt)
+                counter += 1
+                if counter % 1000 == 0:
+                    print(f"Average speed (last 1000): {1/np.mean(timing_info)} Hz \n")
+                    timing_info.clear()
+
 
         visu_3d.start_in_parallel_with(main_loop)
